4ecf1b9dcb007b5af444d70a71fae8bfe6d6e82c
Jordi Vermeulen <j.l.vermeulen@gmail.com>
2015-09-18 19:13:31 +0200

    More genericity, correct pointer type

--- template.cpp
+++ template.cpp
@@ -176,14 +176,6 @@ void swap()
 
 #endif
 
-#ifdef ENABLECACHETEST
-#define READDELAY Sleep( 0 );
-#else
-#define READDELAY ;
-#endif
-int ReadFromRAM( int* address ) { READDELAY return *address; }
-void WriteToRAM( int* address, int value ) { READDELAY *address = value; }
-
 int main( int argc, char **argv ) 
 {  
 #ifdef _MSC_VER
